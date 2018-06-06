6248edd901b38fe0c262282005512a6a09a02da5
HamishClulee <hamish.clulee@hotmail.com>
2018-06-06 12:00:01 +1200

    Reconfigured nginx on server.. . Deployed: 2018-06-06 12:00:01

--- server/server.js
+++ server/server.js
@@ -7,19 +7,20 @@ let colors = require('colors')
 let morgan = require('morgan')
 let cookieParser = require('cookie-parser')
 let bodyParser = require('body-parser')
-// let cors = require('cors')
+let cors = require('cors')
 let configDB = require('./config/database.js')
 let passport = require('passport')
 let flash = require('connect-flash')
 let session = require('express-session')
 let MongoStore = require('connect-mongo')(session)
+let history = require('connect-history-api-fallback')
 let _static = express.static('public')
 let compression = require('compression')
 
 // logging ====================================================================
-// if (process.env.NODE_ENV === 'production') {
-//     app.use(require('morgan')('combined'))
-// }
+if (process.env.NODE_ENV === 'production') {
+    app.use(require('morgan')('combined'))
+}
 
 // configuration ==============================================================
 // require('./config/passport')(passport)
@@ -29,28 +30,28 @@ app.use(compression())
 app.use(cookieParser())
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
-// app.use(cors({
-//     origin: process.env.NODE_ENV === 'development' ? 'http://localhost:1234' : 'https://more-art-than.science',
-//     credentials: true
-// }))
-// app.use(session({
-//     secret: '*&^*&JUKIHKJHG*&YIUHFIUEW*^F*&GHKJ',
-//     name: 'mat-auth-token',
-//     store: new MongoStore({ mongooseConnection: mongoose.connection }),
-//     proxy: true,
-//     resave: true,
-//     saveUninitialized: true,
-//     maxAge: 7200000,
-//     secure: process.env.NODE_ENV === 'development' ? false : true
-// }))
+app.use(cors({
+    origin: process.env.NODE_ENV === 'development' ? 'http://localhost:1234' : 'https://more-art-than.science',
+    credentials: true
+}))
+app.use(session({
+    secret: '*&^*&JUKIHKJHG*&YIUHFIUEW*^F*&GHKJ',
+    name: 'mat-auth-token',
+    store: new MongoStore({ mongooseConnection: mongoose.connection }),
+    proxy: true,
+    resave: true,
+    saveUninitialized: true,
+    maxAge: 7200000,
+    secure: process.env.NODE_ENV === 'development' ? false : true
+}))
 
-// if (app.get('env') === 'production') {
-//     app.set('trust proxy', 1)
-// }
+if (app.get('env') === 'production') {
+    app.set('trust proxy', 1)
+}
 
-// app.use(passport.initialize())
-// app.use(passport.session())
-// app.use(flash())
+app.use(passport.initialize())
+app.use(passport.session())
+app.use(flash())
 app.set('view engine', 'ejs')
 
 // ROUTES =====================================================================
@@ -62,10 +63,10 @@ app.use('/', require('./app/routes/auth'))
 app.use(_static)
 
 // SETTINGS ===================================================================
-// app.use(history({
-//     verbose: true,
-//     disableDotRule: true
-// }))
+app.use(history({
+    verbose: true,
+    disableDotRule: true
+}))
 app.get('*', _static)
 
 // launch =====================================================================
