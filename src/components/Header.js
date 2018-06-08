49336ed251aed93a769249a216c99b6618d39bc3
HamishClulee <hamish.clulee@hotmail.com>
2018-06-08 13:31:18 +1200

    Sass transitions for home header router stubbed out . Deployed: 2018-06-08 13:31:18

--- src/components/Header.js
+++ src/components/Header.js
@@ -3,12 +3,17 @@ import border from '../static/image2vector.svg'
 
 const Header = () => (
   <header className="header-container">
-    <div className="row-1"><div className="row-1-1"></div><div className="row-1-2"></div></div>
-    <div className="row-2"><div className="row-2-1"></div><div className="row-2-2"></div></div>
-      <div className="text-container">
-          <h1 className="lighten">Welcome to M~A~T~S</h1>
-          <h6 className="lighten">Home of Hamish Clulee</h6>
-      </div>
+    <div className="row-1">
+        <div className="row-1-1"><h6>BLOG</h6></div>
+        <div className="row-1-2">
+            <div className="text-container">
+                <h1>Welcome to M~A~T~S</h1>
+                <h3>Home of Hamish Clulee</h3>
+            </div>
+        </div>
+    </div>
+    <div className="row-2"><div className="row-2-1"><h6>WORK</h6></div><div className="row-2-2"><h6>RESUME</h6></div></div>
+
     <div className="border-container">
         <img src={ border } height="180" />
     </div>
@@ -16,5 +21,3 @@ const Header = () => (
 )
 
 export default Header
-
-//#FFC107 #388E3C #1976D2 #D32F2F
--- src/index.js
+++ src/index.js
@@ -4,25 +4,22 @@ import App from './App/'
 import './style/index.sass'
 import store from './store/'
 
-let fetchOk = false
 const baseURL = process.env.NODE_ENV === 'development' ? 'http://localhost:5555' : 'https://more-art-than.science'
 
 fetch(`${baseURL}/api/session_challenge`,  {
-    method: 'GET',
-    credentials: 'include'
-})
-.then(function(response) {
-    return response.json();
-})
-.then(function(resp) {
-    fetchOk = resp.code === 'OK1' ? true : false
-    console.log(fetchOk)
-    init(fetchOk)
-});
+        method: 'GET',
+        credentials: 'include'
+    })
+    .then((response) => {
+        return response.json()
+    })
+    .then((resp) => {
+        init(resp.code === 'OK1')
+    }
+)
 
 
-function init(goBerries) {
-    console.log('berries', goBerries)
+const init = (goBerries) => {
     render(
         <div id="app">
             <App showMain={goBerries} />
