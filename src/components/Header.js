ff582cd7cd4bb5f13b7e5162fa0d538168015def
HamishClulee <hamish.clulee@hotmail.com>
2018-06-10 12:08:06 +1200

    Tagged tiled header.

--- src/components/Header.js
+++ src/components/Header.js
@@ -12,7 +12,7 @@ const Header = () => (
                 <h4>Welcome to M~A~T~S</h4>
                 <h5>Home of Hamish Clulee</h5>
                 <p>I'm a software engineer and web dev from New Zealand. I'm full stack approved, but am most commonly found working on front end challenges, especially UI/UX design and build and Front End Tooling.</p>
-                <div className="header-icons">
+                <div className="header-icons-home">
                     <a href="https://github.com/HamishClulee" target="_blank" className="icon-container-item">
                         <img src={git} height="30"/>
                     </a>
