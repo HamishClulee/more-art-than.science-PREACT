0fdb475f8c348bd9ddeaa80c3aa1969076fb7d60
HamishClulee <hamish.clulee@hotmail.com>
2018-06-11 15:18:47 +1200

    Move anime init to inside mounted hook in component . Deployed: 2018-06-11 15:18:47

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,39 +1,72 @@
-import { h } from 'preact'
-import { Link } from 'preact-router/match';
+import { h, render, Component } from 'preact'
+import { Link } from 'preact-router/match'
 import git from '../static/github.svg'
 import linkedIn from '../static/linkdin.svg'
+import anime from "animejs";
 
-const Header = () => (
-  <header className="header-container">
-    <div className="row-1">
-        <h1 className="ml15">
-            <span className="word">More</span>
-            <span className="word">Art</span>
-            <span className="word">Than</span>
-            <span className="word">Science</span>
-        </h1>
-        <div className="extra-info-container">
-            <div className="text-container">
-                <h4>Welcome to M~A~T~S</h4>
-                <h5>Home of Hamish Clulee</h5>
-                <p>I'm a software engineer and web dev from New Zealand. I'm full stack approved, but am most commonly found working on front end challenges, especially UI/UX design and build and Front End Tooling.</p>
-                <div className="header-icons-home">
-                    <a href="https://github.com/HamishClulee" target="_blank" className="icon-container-item">
-                        <img src={git} height="30"/>
-                    </a>
-                    <a href="https://www.linkedin.com/in/hamish-clulee-163b1950/" target="_blank" className="icon-container-item">
-                        <img src={linkedIn} height="30" />
-                    </a>
+module.exports = class Header extends Component {
+
+    // Vue data
+    constructor() {
+        super();
+    }
+
+    // Vue mounted
+    componentDidMount() {
+        anime.timeline({loop: true})
+            .add({
+                targets: '.ml15 .word',
+                scale: [14,1],
+                opacity: [0,1],
+                easing: "easeOutCirc",
+                duration: 1000,
+                delay: function(el, i) {
+                    return 1000 * i;
+                }
+            }).add({
+            targets: '.ml15',
+            opacity: 0,
+            duration: 3000,
+            easing: "easeOutExpo",
+            delay: 1000
+        });
+    }
+
+    // Vue beforeDestroy
+    componentWillUnmount() {
+    }
+
+    // Vue Template
+    render(props, state) {
+        return <header className="header-container">
+            <div className="row-1">
+                <h1 className="ml15">
+                    <span className="word">More</span>
+                    <span className="word">Art</span>
+                    <span className="word">Than</span>
+                    <span className="word">Science</span>
+                </h1>
+                <div className="extra-info-container">
+                    <div className="text-container">
+                        <h4>Welcome to M~A~T~S</h4>
+                        <h5>Home of Hamish Clulee</h5>
+                        <p>I'm a software engineer and web dev from New Zealand. I'm full stack approved, but am most commonly found working on front end challenges, especially UI/UX design and build and Front End Tooling.</p>
+                        <div className="header-icons-home">
+                            <a href="https://github.com/HamishClulee" target="_blank" className="icon-container-item">
+                                <img src={git} height="30"/>
+                            </a>
+                            <a href="https://www.linkedin.com/in/hamish-clulee-163b1950/" target="_blank" className="icon-container-item">
+                                <img src={linkedIn} height="30" />
+                            </a>
+                        </div>
+                    </div>
                 </div>
             </div>
-        </div>
-    </div>
-    <div className="row-2">
-        <div className="row i2-1"><Link href="/work"><h5>WORK</h5></Link></div>
-        <div className="row i2-2"><Link href="/blog"><h5>BLOG</h5></Link></div>
-        <div className="row i2-3"><Link href="/tech"><h5>IPSEITY</h5></Link></div>
-    </div>
-  </header>
-)
-
-export default Header
+            <div className="row-2">
+                <div className="row i2-1"><Link href="/work"><h5>WORK</h5></Link></div>
+                <div className="row i2-2"><Link href="/blog"><h5>BLOG</h5></Link></div>
+                <div className="row i2-3"><Link href="/tech"><h5>IPSEITY</h5></Link></div>
+            </div>
+        </header>;
+    }
+}
--- src/index.js
+++ src/index.js
@@ -1,5 +1,4 @@
 import 'preloadfill'
-import anime from 'animejs'
 import  { h, render } from 'preact'
 import App from './App/'
 import './style/index.sass'
@@ -12,21 +11,3 @@ render(
     </div>,
     document.body
 )
-
-anime.timeline({loop: true})
-    .add({
-        targets: '.ml15 .word',
-        scale: [14,1],
-        opacity: [0,1],
-        easing: "easeOutCirc",
-        duration: 1000,
-        delay: function(el, i) {
-            return 1000 * i;
-        }
-    }).add({
-    targets: '.ml15',
-    opacity: 0,
-    duration: 3000,
-    easing: "easeOutExpo",
-    delay: 1000
-});
